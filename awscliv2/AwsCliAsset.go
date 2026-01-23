3df872c0f10991041ca4d57f2985c794afc5e6d0
AWS CDK Team <aws-cdk@amazon.com>
2026-01-23 00:45:33 +0000

    chore(release): v2.0.159

--- awscliv2/AwsCliAsset.go
+++ awscliv2/AwsCliAsset.go
@@ -64,6 +64,8 @@ type AwsCliAsset interface {
 	//
 	AddResourceMetadata(resource awscdk.CfnResource, resourceProperty *string)
 	// Grants read permissions to the principal on the assets bucket.
+	//
+	// [disable-awslint:no-grants].
 	GrantRead(grantee awsiam.IGrantable)
 	// Returns a string representation of this construct.
 	ToString() *string
