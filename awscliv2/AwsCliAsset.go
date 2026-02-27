15155ad1d40d8c25bb9f812d8192a507a50dead7
AWS CDK Team <aws-cdk@amazon.com>
2026-02-27 00:52:18 +0000

    chore(release): v2.0.164

--- awscliv2/AwsCliAsset.go
+++ awscliv2/AwsCliAsset.go
@@ -69,6 +69,15 @@ type AwsCliAsset interface {
 	GrantRead(grantee awsiam.IGrantable)
 	// Returns a string representation of this construct.
 	ToString() *string
+	// Applies one or more mixins to this construct.
+	//
+	// Mixins are applied in order. The list of constructs is captured at the
+	// start of the call, so constructs added by a mixin will not be visited.
+	// Use multiple `with()` calls if subsequent mixins should apply to added
+	// constructs.
+	//
+	// Returns: This construct for chaining.
+	With(mixins ...constructs.IMixin) constructs.IConstruct
 }
 
 // The jsii proxy struct for AwsCliAsset
@@ -268,6 +277,24 @@ func (a *jsiiProxy_AwsCliAsset) ToString() *string {
 		a,
 		"toString",
 		nil, // no parameters
+		&returns,
+	)
+
+	return returns
+}
+
+func (a *jsiiProxy_AwsCliAsset) With(mixins ...constructs.IMixin) constructs.IConstruct {
+	args := []interface{}{}
+	for _, a := range mixins {
+		args = append(args, a)
+	}
+
+	var returns constructs.IConstruct
+
+	_jsii_.Invoke(
+		a,
+		"with",
+		args,
 		&returns,
 	)
 
